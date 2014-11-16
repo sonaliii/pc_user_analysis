c9863e81c588f4f0d70ba04191498be0fdc623d7
sonaliii <sona8242@gmail.com>
2014-11-15 19:20:33 -0800

    pep8 improvements

--- captions.py
+++ captions.py
@@ -4,12 +4,12 @@ Imports caption text data, groups by User ID, and saves to CSV
 '''
 lines = []
 with open('../data/captions.txt') as f:
-  lines = f.readlines()
+    lines = f.readlines()
 
 split_lines = []
 for line in lines:
-  split_line = line.split(',', 2)
-  split_lines.append(split_line)
+    split_line = line.split(',', 2)
+    split_lines.append(split_line)
 
 df = pd.DataFrame(split_lines, columns=['user_id', 'circle_id', 'caption'])
 df = df.drop('circle_id', axis=1)
@@ -18,4 +18,3 @@ joined = grouped.aggregate({'caption': ' '.join})
 joined = joined.reset_index()
 
 joined.to_csv('../data/captions.csv')
-
--- referrals.py
+++ referrals.py
@@ -17,18 +17,30 @@ import seaborn
 
 class Referrals(object):
     def __init__(self):
-        self.user_df = pd.read_csv('../data/u.csv', sep=',', error_bad_lines=False, dtype={'UserID': 'object', 'SourceUserID': 'object', 'Email': 'object', 'FacebookID': 'object', 'CircleIDs': 'object'})
-        self.weeks = pd.read_csv('../data/week_counts.csv', sep=',', error_bad_lines=False, header=None)
+        self.user_df = pd.read_csv('../data/u.csv',
+                                   sep=',',
+                                   error_bad_lines=False,
+                                   dtype={'UserID': 'object',
+                                          'SourceUserID': 'object',
+                                          'Email': 'object',
+                                          'FacebookID': 'object',
+                                          'CircleIDs': 'object'})
+        self.weeks = pd.read_csv('../data/week_counts.csv',
+                                 sep=',',
+                                 error_bad_lines=False,
+                                 header=None)
         self.weeks.columns = ['user_id', 'weeks']
         self.users = None
 
     def join_referrals_weeks(self):
         '''
-        Returns a dataframe with user IDs and the number of referral weeks they have 
-        (the number of total weeks spent on the app by everyone they have referred/invited)
+        Returns a dataframe with user IDs and the number of referral weeks
+        they have (the number of total weeks spent on the app by everyone
+        they have referred/invited)
         '''
         referrals = self.user_df[['UserID', 'SourceUserID']].dropna()
-        referral_weeks = referrals.merge(self.weeks, how='inner', left_on='UserID', right_on='user_id')
+        referral_weeks = referrals.merge(self.weeks, how='inner',
+                                         left_on='UserID', right_on='user_id')
         referral_weeks = referral_weeks.groupby('SourceUserID')
         summed = referral_weeks.aggregate({'weeks': np.sum})
         summed = summed.reset_index()
@@ -49,34 +61,33 @@ class Referrals(object):
     def priority_by_weeks(self, referral_weeks_df):
         referrers = referral_weeks_df
         #Setting priority users to those who have referred at least 10 other users
-        referrers['Priority'] = referrers['Referrals'].apply(lambda x: True if x >= 80 else False)
-         
+        referrers['Priority'] = referrers['Referrals'].apply(lambda x: x >= 80)
+
         #Merging all users with those who have referred others
         users = self.user_df.merge(referrers, how='outer', left_on='UserID', right_on='UserID')
-        
+
         #Replacing NAs with 0 referrals and False priority
         users['Referrals'] = users['Referrals'].fillna(0)
         users['Priority'] = users['Priority'].fillna(False)
-        
+
         self.users = users
         return users
-
 
     def count_referrals(self):
         users = self.user_df
         referrals = users[['UserID', 'SourceUserID']]
         referrals = referrals.dropna()
-        
+
         #Counting the number of occurrences of a referral by each SourceUserID
         referrers = referrals.groupby('SourceUserID').count()
-        
+
         #Removing repeated column and sorting by number of referrals
-        referrers = referrers.drop('SourceUserID', axis = 1)
+        referrers = referrers.drop('SourceUserID', axis=1)
         referrers = referrers.sort(columns='UserID', ascending=False)
-        
+
         #Resetting index to make SourceUserID a column again
         referrers = referrers.reset_index()
-        
+
         #Renaming column with count of referrals
         referrers.columns = ['SourceUserID', 'Referrals']
 
@@ -84,28 +95,28 @@ class Referrals(object):
 
     def identify_priority_users(self, referrers_df):
         referrers = referrers_df
-        #Setting priority users to those who have referred at least 10 other users
-        referrers['Priority'] = referrers['Referrals'].apply(lambda x: True if x >= 10 else False)
-        
+        #Setting priority users to those who have referred at least 10 others
+        referrers['Priority'] = referrers['Referrals'].apply(lambda x: x >= 10)
+
         #Trying various limits for priority number of referrals
         for i in range(10):
-            referrers['Priority' + str(i + 1)] = referrers['Referrals'].apply(lambda x: True if x >= i + 1 else False)
-    
+            referrers['Priority' + str(i + 1)] = referrers['Referrals'].apply(lambda x: x >= i + 1)
+
         #Merging all users with those who have referred others
         users = self.user_df.merge(referrers, how='outer', left_on='UserID', right_on='SourceUserID')
-        users = users.drop('SourceUserID_x', axis = 1)
-        users = users.drop('SourceUserID_y', axis = 1)
-        
+        users = users.drop('SourceUserID_x', axis=1)
+        users = users.drop('SourceUserID_y', axis=1)
+
         #Replacing NAs with 0 referrals and False priority
         users['Referrals'] = users['Referrals'].fillna(0)
         users['Priority'] = users['Priority'].fillna(False)
-        
+
         self.users = users
         return users
 
     def count_circles(self, priority_users):
         users = self.users
-        
+
         #Counting the number of circles each user belongs to
         users['has_circles'] = users['CircleIDs'].apply(lambda x: not isinstance(x, float))
         users['Circles'] = users['CircleIDs'][users['has_circles'] == True].apply(lambda x: len(x.split(';')))
@@ -114,12 +125,12 @@ class Referrals(object):
 
     def plot_referrals_distribution(referrers):
         #Looking at the distribution of the number of referrals per user
-        hist(referrers['Referrals'], bins = 100)
+        hist(referrers['Referrals'], bins=100)
         plt.title('Number of referrals per user')
         plt.xlabel('Number referred')
         plt.ylabel('Number of users')
-    
-if __name__=='__main__':
+
+if __name__ == '__main__':
     r = Referrals()
     # referrers = r.join_referrals_weeks()
     # retention = r.priority_retention()
